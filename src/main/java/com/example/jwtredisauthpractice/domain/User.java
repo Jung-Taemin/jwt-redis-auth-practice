0ca18bcdf88d33f32547a4f547419c5d47f1636a
taemin <jungtaemin1214@naver.com>
2025-09-29 18:34:58 +0900

    changes

---
+++ src/main/java/com/example/jwtredisauthpractice/domain/User.java
@@ -0,0 +1,26 @@
+package com.example.jwtredisauthpractice.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+}
