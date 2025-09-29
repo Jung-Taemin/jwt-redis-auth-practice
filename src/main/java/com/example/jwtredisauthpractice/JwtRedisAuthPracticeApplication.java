8744c609b480a229920fa5e8e0cf98caf64e50af
taemin <jungtaemin1214@naver.com>
2025-09-29 18:24:39 +0900

    changes

---
+++ src/main/java/com/example/jwtredisauthpractice/JwtRedisAuthPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtredisauthpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtRedisAuthPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtRedisAuthPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jwtredisauthpractice/JwtRedisAuthPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtredisauthpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtRedisAuthPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
